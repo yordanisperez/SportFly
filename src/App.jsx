8ab7b8336a72e267b32a0eea08a49b12a70f4d3a
yordanisperez <yordanis@gmail.com>
2021-08-26 15:20:18 -0400

    Arreglos al cargar los datos del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState,useEffect,Fragment} from 'react'
+import {Fragment} from 'react'
 import css from  './index.scss' 
 import {Route,Switch} from 'react-router-dom';
 
@@ -7,53 +7,14 @@ import Login from './component/Login'
 import logo from './LogoPalomaOpt.svg'
 import { useAuth0 } from "@auth0/auth0-react";
 import Backdrop from './component/Backdrop';
-import regeneratorRuntime from "regenerator-runtime";
+import Infouser from './component/Infouser'
 
-const domain=process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId=process.env.REACT_APP_AUTH0_CLIENT_ID;
-const audience=process.env.REACT_APP_AUTH0_AUDIENCE
 
 
 
 const App=()=>{
-    const { isLoading,user, getAccessTokenSilently,isAuthenticated} = useAuth0();
-    const [userMetadata, setUserMetadata] = useState(null);
-
-//dev-csgmhfrr.us.auth0.com
-
-    useEffect(() => {
-        const getUserMetadata = async () => {
-
-          try {
-            const accessToken = await getAccessTokenSilently({
-              audience:`https://${domain}/api/v2/`,
-              scope: "read:current_user",
-            });
-          //  console.log("Token de Access: ",accessToken);
-            const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-         //   console.log(userDetailsByIdUrl);
-            const metadataResponse = await fetch(userDetailsByIdUrl, {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-              },
-              
-            });
-      
-            const { user_metadata } = await metadataResponse.json();
-      
-            setUserMetadata(user_metadata);
-          } catch (e) {
-            console.log(e.message);
-          }
-        };
-      
-        getUserMetadata();
-      }, [getAccessTokenSilently, user?.sub]);
-
-
-
-
-   
+    const { isLoading} = useAuth0();
+ 
     if (isLoading) {
         return <div className="welcome-section"> 
                 <Backdrop>
@@ -68,24 +29,9 @@ const App=()=>{
         <Layout>
           <Switch>
             <Route path="/" exact>
-              <div className="welcome-section">
-                {isAuthenticated && (
-                  <div>
-                    <p>{JSON.stringify(user).replace(/,/gi, ", ")}</p>
-                    <h3>User Metadata</h3>
-                    {userMetadata ? (
-                      <pre>
-                        {JSON.stringify(userMetadata, null, 2).replace(
-                          /,/gi,
-                          ", "
-                        )}
-                      </pre>
-                    ) : (
-                      "No user metadata defined"
-                    )}
-                  </div>
-                )}
-              </div>
+              <Infouser>
+ 
+              </Infouser>
             </Route>
             <Route path="/login">
               <Login
---
+++ src/component/Infouser.js
@@ -0,0 +1,66 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import {useState,useEffect,Fragment} from 'react'
+import regeneratorRuntime from "regenerator-runtime";
+
+const domain=process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId=process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience=process.env.REACT_APP_AUTH0_AUDIENCE
+
+
+
+
+function Infouser(props){
+    const { user, getAccessTokenSilently,isAuthenticated} = useAuth0();
+    const [userMetadata, setUserMetadata] = useState(null);
+    useEffect(() => {
+        const getUserMetadata = async () => {
+
+          try {
+            const accessToken = await getAccessTokenSilently({
+              audience:`https://${domain}/api/v2/`,
+              scope: "read:current_user",
+            });
+          //  console.log("Token de Access: ",accessToken);
+            const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+         //   console.log(userDetailsByIdUrl);
+            const metadataResponse = await fetch(userDetailsByIdUrl, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+              
+            });
+      
+            const { user_metadata } = await metadataResponse.json();
+      
+            setUserMetadata(user_metadata);
+          } catch (e) {
+            console.log(e.message);
+          }
+        };
+      
+        getUserMetadata();
+      }, [getAccessTokenSilently, user?.sub]);
+
+
+
+        return    <div className="welcome-section">
+                    {isAuthenticated && (
+                    <div>
+                        <p>{JSON.stringify(user).replace(/,/gi, ", ")}</p>
+                        <h3>User Metadata</h3>
+                        {userMetadata ? (
+                        <pre>
+                            {JSON.stringify(userMetadata, null, 2).replace(
+                            /,/gi,
+                            ", "
+                            )}
+                        </pre>
+                        ) : (
+                        "No user metadata defined"
+                        )}
+                    </div>
+                    )}
+                </div>
+}
+
+export default Infouser;
